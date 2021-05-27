c76921e717f5b179033b2c7d115d7e4254b26bc4
john <johnthomas222000@gmail.com>
2021-05-27 18:27:15 +0530

    Completed Upto buttons working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import Todo from "./Todo";
 
-  function TodoList() {
-    return (
-        <div className="todo-container">
-        <ul className="todo-list"></ul>
-      </div>
-    )
+function TodoList({ todos, setTodos }) {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            text={todo.text}
+            todo={todo}
+            key={todo.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default TodoList;
