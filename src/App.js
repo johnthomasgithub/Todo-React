9a15f57afadae202bd2dc7974f07e69103b0f454
john <johnthomas222000@gmail.com>
2021-05-27 12:23:10 +0530

    public index.html Modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-     
+      <header>
+        <h1>Hello John</h1>
+      </header>
+      <Form/>
     </div>
   );
 }
