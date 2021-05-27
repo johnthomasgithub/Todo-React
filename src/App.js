66f147d02afdfdc3999d8fa06ef7575227502b5d
john <johnthomas222000@gmail.com>
2021-05-27 22:44:37 +0530

    Filter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText] = useState(""); 
-  const [todos,setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
   return (
     <div className="App">
       <header>
         <h1>Hello John</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
