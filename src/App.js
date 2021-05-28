9dfcef55c9ec1a3b735c404f77ed572a8fc9eaad
john <johnthomas222000@gmail.com>
2021-05-28 09:09:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ function App() {
   const [status, setStatus] = useState("all");
   const [filteredTodos,setFilteredTodos] = useState([]);
 
+  useEffect(() =>{
+    getLocalTodos();
+  },[])
+
   useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos,status]);
 
 
@@ -27,7 +32,20 @@ function App() {
         setFilteredTodos(todos);
         break;  
     }
+  };
+  const saveLocalTodos = () =>{
+        localStorage.setItem('todos',JSON.stringify(todos));
   }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null)
+      localStorage.setItem('todos', JSON.stringify([]));
+      else{
+        let todoLocal = JSON.parse(localStorage.getItem("todos"))
+        setTodos(todoLocal);
+      }
+  }
+
   return (
     <div className="App">
       <header>
@@ -39,9 +57,9 @@ function App() {
         setTodos={setTodos}
         setInputText={setInputText}
         setStatus={setStatus}
-        filteredTodos={filteredTodos}
+       
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList  filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
 }
