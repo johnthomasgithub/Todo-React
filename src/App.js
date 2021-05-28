a9e29c919161cb8b45ab71e6229c71136e8c9861
john <johnthomas222000@gmail.com>
2021-05-28 15:43:45 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Hello John</h1>
+        <h1>My Todo's</h1>
       </header>
       <Form
         inputText={inputText}
